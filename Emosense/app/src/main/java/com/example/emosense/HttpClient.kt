6a061f93dfc7b33358f4b0b5b05f549e50184fa6
Saransh Baniyal <saranshbaniyal14@gmail.com>
2023-10-11 14:23:54 +0530

    config changes

--- Emosense/app/src/main/java/com/example/emosense/HttpClient.kt
+++
@@ -1,8 +0,0 @@
-package com.example.emosense
-import okhttp3.OkHttpClient
-
-object HttpClient {
-    val baseurl = "https://b1e1-220-158-168-162.ngrok-free.app"
-    lateinit var username: String
-    val client = OkHttpClient()
-}
